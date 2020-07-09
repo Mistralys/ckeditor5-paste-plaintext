90c198605642bbef5e8db1428d7fc2020d204fc4
Mistralys <s.mordziol@gmail.com>
2020-07-09 09:34:12 +0200

    Added translations for the button, added button icon.

--- src/ui.js
+++ src/ui.js
@@ -1,6 +1,28 @@
 
 import Plugin from '@ckeditor/ckeditor5-core/src/plugin';
 import ButtonView from '@ckeditor/ckeditor5-ui/src/button/buttonview';
+import PasteIcon from './paste-plain.svg';
+
+/**
+ * Import the translation service, so we can add some 
+ * translations for the button text.
+ */
+import { add } from '@ckeditor/ckeditor5-utils/src/translation-service';
+
+add(
+    'de',
+    {
+        'Toggle removing any formatting from pasted text.': 'Das Entfernen der Formatierungen beim Einfügen ein- oder ausschalten.'
+    }
+);
+
+add(
+    'fr',
+    {
+        'Toggle removing any formatting from pasted text.': 'Activer ou désactiver la suppression des formatages du texte à l\'insertion.'
+    }
+);
+
 
 /**
  * Handles registering the toggleable button in the 
@@ -14,12 +36,16 @@ export default class PastePlainTextUI extends Plugin {
         editor.ui.componentFactory.add( 'pastePlainText', locale => {
             const view = new ButtonView( locale );
             const command = editor.commands.get( 'pastePlainText' );
+            
+            // to be table to use the translation function
+            const t = view.t; 
 
             view.set( {
-                label: 'Paste as plain text',
-                withText: true,
+                label: t('Toggle removing any formatting from pasted text.'),
+                withText: false,
                 tooltip: true,
-                isToggleable: true
+                isToggleable: true,
+                icon:PasteIcon
             } );
 
             // Callback executed once the button is clicked.
